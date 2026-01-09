eefa80ed81383cceb9edc967b8b8fb0641120f9a
samchat2561 <samchat2561@gmail.com>
2026-01-09 11:00:38 +0700

    update this keyword

--- lib/dog.dart
+++ lib/dog.dart
@@ -3,20 +3,22 @@ class Dog {
   final String? name;
   final int? age;
   final int? dogId;
+  String? breed;
 
   //2.Constant constructor
-  Dog({this.name, this.age, this.dogId});
+  Dog({this.name, this.age, this.dogId, this.breed});
 
   //3.Method/function
   void dogInfo() {
     print("Dog of Name: $name");
     print("Dog of age: $age");
     print("Dog of dogId: $dogId");
+    print("Dog of dogId: $breed");
   }
 }
 
 void callDog() {
   //Instance object of dog class
-  final Dog dog = Dog(name: "Thong", age: 20, dogId: 101);
+  final Dog dog = Dog(name: "Thong", age: 20, dogId: 101, breed: "Blue dog");
   dog.dogInfo();
 }
